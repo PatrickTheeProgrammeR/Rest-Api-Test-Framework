e66a2efac7e9f70836ffcdd21ed35c5735425cd6
Patryk J <patrykytgamer@gmail.com>
2026-06-16 14:16:05 +0200

    Dodanie Pydantic, models i plików init

---
+++ api_tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from dotenv import load_dotenv
+import os
+from clients.user_client import UserClient
+
+@pytest.fixture(scope="session")
+def client():
+    load_dotenv()
+    base_url = os.getenv("BASE_URL")
+    token = os.getenv("API_KEY")
+    return UserClient(base_url, token)
---
+++ api_tests/models/user_model.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class UserData(BaseModel):
+    id : int
+    email : str
+    first_name : str
+    last_name : str
+
+
+class UserResponse(BaseModel):
+    data : UserData
